30193720d5b9feaea358b4bdec506b4f7aa2b1a0
muraokad <muraokad@msu.edu>
2016-12-09 20:01:38 -0500

    More Changes

--- js/final.js
+++ js/final.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var img = document.getElementById('Aur');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -15,6 +15,6 @@ img.onclick = function(){
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
 }
